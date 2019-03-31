b0b028ec978c974b8b1d6b3d52b69d32f86c23fd
173882121 <173882121@qq.com>
2019-03-31 22:47:26 +0800

    Add files via upload

---
+++ TradeTheResource/TradeTheResource.Web/App_Data/AuthenticationAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TradeTheResource.Models;
+
+
+namespace TradeTheResource.Web
+{
+    public class AuthenticationAttribute : ActionFilterAttribute
+    {
+
+       public UserEntity userEntity { get; set; }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            if (filterContext.HttpContext.Session["user"] == null)
+            {
+                filterContext.Result = new RedirectResult("/Home/Index", true);
+                return;
+            }
+            else
+
+            {
+                userEntity = filterContext.HttpContext.Session["user"] as UserEntity;
+
+            }
+            
+
+
+            base.OnActionExecuting(filterContext);
+        }
+
+    }
+}
---
+++ TradeTheResource/TradeTheResource.Web/App_Data/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TradeTheResource.Models;
+
+namespace TradeTheResource.Web
+{
+    public class BaseController :Controller
+    {
+        public UserEntity userEntity { get; set; }
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            if (filterContext.HttpContext.Session["user"] == null)
+            {
+                filterContext.Result = new RedirectResult("/Home/Index", true);
+                return;
+            }
+            else
+
+            {
+                userEntity = filterContext.HttpContext.Session["user"] as UserEntity;
+
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
